631124dd99b635b9b71893a9443a65a3f44ec7c1
cuijiexiang <1484895345@qq.com>
2024-04-23 14:29:19 +0800

    缩小训练集，使用split划分训练集与验证集、增加更多显示回调

--- transformer/run.py
+++ transformer/run.py
@@ -23,48 +23,10 @@ def list_files_and_directories(start_path, indent=0):
             print(f"{prefix}    文件: {item}")
 
 
-def copy_directory_contents(src_dir, dst_dir=None):
-    """
-    Copies all the files from the source directory to a new directory.
-
-    Args:
-    src_dir (str): The path to the source directory.
-    dst_dir (str): The path to the destination directory. If None, it defaults to src_dir + 'copy'.
-
-    Returns:
-    None
-    """
-    # 如果没有提供目标目录，则创建一个默认的复制目录
-    if dst_dir is None:
-        dst_dir = src_dir + "copy"
-
-    # 检查源目录是否存在
-    if not os.path.exists(src_dir):
-        raise ValueError("Source directory does not exist")
-
-    # 创建目标目录
-    if not os.path.exists(dst_dir):
-        os.makedirs(dst_dir)
-
-    # 遍历源目录及其所有子目录
-    for root, dirs, files in os.walk(src_dir):
-        # 计算目标目录路径
-        destination_path = root.replace(src_dir, dst_dir, 1)
-
-        # 确保目标路径的目录存在
-        if not os.path.exists(destination_path):
-            os.makedirs(destination_path)
-
-        # 复制每个文件到新目录
-        for file in files:
-            src_file = os.path.join(root, file)
-            dst_file = os.path.join(destination_path, file)
-            shutil.copy(src_file, dst_file)
-
 
 # 定义命令和参数
 command = "python"
-script_path = "../script/.py"
+script_path = "../script/llama3_api_server.py"
 
 base_model_option = "--base_model"
 lora_model_option = "--lora_model"
