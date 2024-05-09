eb4820d9cb580611beb9d324c41aa33d65bf2b6f
cuijiexiang <1484895345@qq.com>
2024-05-09 16:58:17 +0800

    缩小训练集，使用split划分训练集与验证集、增加更多显示回调

--- train/train.py
+++ train/train.py
@@ -42,8 +42,8 @@ deepspeed_config_file = "ds_zero2_no_offload.json"
 additional_path = "models--mistralai--Mistral-7B-Instruct-v0.2/snapshots/41b61a33a2483885c981aa79e0df6b32407ed873"
 tokenizer_path = model_path = model_utils.get_model_file_path()  # 模型路径
 # tokenizer_path = model_path = '/result/Mistral-7B-final-v4'
-output_dir = "/result/LLama3-8B/lora"  # lora模型输出路径
-output_model_final = model_utils.get_model_output_path() # 合并后模型输出路径
+# output_dir = "/result/LLama3-8B/lora"  # lora模型输出路径
+output_dir = model_utils.get_model_output_path() # 合并后模型输出路径
 
 print(f"model_path is : {model_path}")
 
